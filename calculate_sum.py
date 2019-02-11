4e2ec4024e868056e8bb5f2d71ad3e04128cd1dd
Bhuvaneshwaran-Sethuraman <47036251+Bhuvaneshwaran-Sethuraman@users.noreply.github.com>
2019-02-11 09:11:45 +0000

    Create calculate_sum.py

---
+++ calculate_sum.py
@@ -0,0 +1,10 @@
+#BHUVANESHWARAN
+#To calculate the sum of natural numbers upto N
+print("INPUT")
+bhu=int(input())
+count=0
+while(bhu>0):
+    count=count+bhu
+    bhu=bhu-1
+print("OUTPUT")
+print(count)
